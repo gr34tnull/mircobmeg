c31dcd66bf0efe94c907e9fa26b0d1d56bc4ec8b
GR34TNULL <gr34tnull@GR34TNULL.local>
2021-03-22 08:07:58 +0800

    complete

--- resources/views/bloodlines/show.blade.php
+++ resources/views/bloodlines/show.blade.php
@@ -179,7 +179,7 @@
                         <div class="flex w-full text-sm text-gray-600">
                             <label for="images" class="w-full px-3 py-2 text-sm font-medium leading-4 text-center text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm cursor-pointer hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                             <span>Upload Images</span>
-                            <input id="images" name="file[]" type="file" class="sr-only" accept="image/*" multiple="multiple" required>
+                            <input id="images" name="file[]" type="file" class="sr-only" accept="image/*" multiple="multiple">
                             </label>
                         <div>
                     </dd>
